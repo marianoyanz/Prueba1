5f29a087d5e2d346ce667c66e54cbbf5600233bd
marianoyanz <marianoyanz@hotmail.com>
2024-03-20 20:22:21 -0300

    tipo de datos

---
+++ datatype.py
@@ -0,0 +1,41 @@
+#string
+print("hello world")
+print('hello world')
+print("""hello world""")
+print('''hello wolrd''')
+
+print(type("hello world"))
+print("bye" + "world")
+
+#numbers
+
+print(30)
+print(type(30))
+print(type(30.5))
+
+#boolean
+False
+True
+
+#list
+
+[10,20,30]
+["hello","world"]
+[]
+
+#tuplas: no se puede cambiar, inmutable type:tuple
+
+(100,20,30)
+()
+
+#diccionaries type:dict
+
+{
+    "nombre":"Ryan",
+    "apelido":"Ray",
+    "apodo":"Fazt"
+}
+
+#None type:None
+
+
